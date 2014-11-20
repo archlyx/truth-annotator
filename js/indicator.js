b51f1ea6a545e363bbe24c6fc6c0ef0cf8ef0ea1
archlyx <archlyx@gmail.com>
2014-11-20 10:50:40 -0600

    Indicator added

---
+++ js/indicator.js
@@ -0,0 +1,32 @@
+/*
+
+  Truth Annotator
+  Version 0.0.1
+  (c) 2014 by Yu, Shuangping
+
+*/
+
+;(function(indicator, $) {
+
+  $.extend(indicator, {
+
+    var box = "<div id=ta-indicator>Opinion Saved</div>";
+    
+    init: function() {
+
+    }，
+
+    makeLinkBox: function() {
+
+    },
+
+    show: function() {
+
+    },
+
+    hide: function() {
+
+    }
+
+  });
+})(window.indicator = window.indicator || {}, jQuery);
